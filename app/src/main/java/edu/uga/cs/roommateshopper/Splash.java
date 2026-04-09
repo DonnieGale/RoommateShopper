df807e4190023d4bdfd62560ab83da762e5643f8
Donald Gale <DonnieGale@Icloud.com>
2026-04-09 18:09:33 -0400

    Menu does stuff

--- app/src/main/java/edu/uga/cs/roommateshopper/Splash.java
+++ app/src/main/java/edu/uga/cs/roommateshopper/Splash.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +19,7 @@ import android.view.ViewGroup;
  */
 public class Splash extends Fragment {
 
+    BottomNavigationView bottomNavigationView;
 
     public Splash() {
         // Required empty public constructor
@@ -44,6 +48,26 @@ public class Splash extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState ) {
         super.onViewCreated(view, savedInstanceState);
+
+        bottomNavigationView = view.findViewById(R.id.bottomNavigationView);
+
+        FragmentManager manager = getActivity().getSupportFragmentManager();
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.action_shoppingList) {
+                manager.beginTransaction().replace(R.id.fragmentContainerView2, new ShoppingList()).commit();
+            } else if (itemId == R.id.action_recentlyPurchased) {
+                //manager.beginTransaction().replace(R.id.fragmentContainerView2, new RecentlyPurchased()).commit();
+            } else if (itemId == R.id.action_prevPurchases) {
+                //manager.beginTransaction().replace(R.id.fragmentContainerView2, new PrevPurchases()).commit();
+            } else if (itemId == R.id.action_roommates) {
+                //manager.beginTransaction().replace(R.id.fragmentContainerView2, new Roommates()).commit();
+            } else if (itemId == R.id.action_profile) {
+                //manager.beginTransaction().replace(R.id.fragmentContainerView2, new ProfileFragment()).commit();
+            }
+            return true;
+        });
 
     }
 }
